5dcdb911363ccb27fa3363ff5a3e849b17d06cc4
Caio <ccardoso.judo@gmail.com>
2023-07-30 21:33:09 -0300

    Commit teste

---
+++ PrimeiroProjetoJava/src/br/com/ccardoso/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.com.ccardoso;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ambiente = args [0];
+		System.out.print("Hello World ");
+		System.out.print(ambiente);
+	}
+
+}
